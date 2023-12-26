13fd3259805c0c81f2b0335a369902a8a7656d44
pakaufmann <patrick-kaufmann@gmx.ch>
2023-12-26 09:45:12 +0100

    Day 25, optimized

--- src/main/scala/Day25.scala
+++ src/main/scala/Day25.scala
@@ -8,70 +8,64 @@ object Day25 {
   }
 
   def part1(connections: Map[String, Set[String]]): Int = {
-    val cons = connections.toList
-      .flatMap { case (name, cons) => cons.map(c => (if (name.compareTo(c) < 0) c else name, if (name.compareTo(c) < 0) name else c)) }
-      .toSet
-
     val (start, end) = findFarthestAway(connections)
 
-    val first = findFastest(start, end, connections)
-    val remaining = first.sliding(2).foldLeft(connections) { case (cons, f :: t :: Nil) =>
-      cons.removeConnection(f, t).removeConnection(t, f)
-    }
+    val split = Iterator
+      .iterate(connections) { c => c.removeConnections(c.findFastest(start, end)) }
+      .dropWhile(_.findGroups()._2.isEmpty)
+      .next()
 
-    val second = findFastest(start, end, remaining)
-    val remaining2 = second.sliding(2).foldLeft(remaining) { case (cons, f :: t :: Nil) =>
-      cons.removeConnection(f, t).removeConnection(t, f)
-    }
-
-    val third = findFastest(start, end, remaining2)
-
-    for (f1 :: t1 :: Nil <- first.sliding(2)) {
-      for (f2 :: t2 :: Nil <- second.sliding(2)) {
-        for (f3 :: t3 :: Nil <- third.sliding(2)) {
-          val removed = connections.removeConnection(f1, t1)
-            .removeConnection(t1, f1)
-            .removeConnection(f2, t2)
-            .removeConnection(t2, f2)
-            .removeConnection(f3, t3)
-            .removeConnection(t3, f3)
-          val (first, second) = findGroups(removed)
-
-          if (second.nonEmpty) {
-            return first.size * second.size
-          }
-        }
-      }
-    }
-
-    0
+    val (f, s) = split.findGroups()
+    f.size * s.size
   }
 
-  implicit class ConnectionOps(connections: Map[String, Set[String]]) {
+  implicit class ConnectionOps(map: Map[String, Set[String]]) {
     def removeConnection(from: String, to: String): Map[String, Set[String]] =
-      connections.updatedWith(from) { cons =>
+      map.updatedWith(from) { cons =>
         Some(cons.getOrElse(Set()) - to)
       }
-  }
 
-  def findFastest(from: String, to: String, connections: Map[String, Set[String]]): List[String] = {
-    val queue = mutable.Queue((from, List[String]()))
-    val visited = mutable.Set[String]()
-
-    while (queue.nonEmpty) {
-      val (component, steps) = queue.dequeue()
-
-      if (component == to) {
-        return steps :+ component
-      }
-
-      if (!visited.contains(component)) {
-        visited.add(component)
-        queue.addAll(connections(component).map(c => (c, steps :+ component)))
+    def removeConnections(connections: List[String]): Map[String, Set[String]] = {
+      connections.sliding(2).foldLeft(map) { case (cons, f :: t :: Nil) =>
+        cons.removeConnection(f, t).removeConnection(t, f)
       }
     }
 
-    List()
+    def findFastest(from: String, to: String): List[String] = {
+      val queue = mutable.Queue((from, List[String]()))
+      val visited = mutable.Set[String]()
+
+      while (queue.nonEmpty) {
+        val (component, steps) = queue.dequeue()
+
+        if (component == to) {
+          return steps :+ component
+        }
+
+        if (!visited.contains(component)) {
+          visited.add(component)
+          queue.addAll(map(component).map(c => (c, steps :+ component)))
+        }
+      }
+
+      List()
+    }
+
+    def findGroups(): (Set[String], Set[String]) = {
+      val queue = mutable.Queue(map.head._1)
+      val visited = mutable.Set[String]()
+
+      while (queue.nonEmpty) {
+        val component = queue.dequeue()
+
+        if (!visited.contains(component)) {
+          visited.add(component)
+          queue.addAll(map(component))
+        }
+      }
+
+      (visited.toSet, map.keySet -- visited)
+    }
   }
 
   def findFarthestAway(connections: Map[String, Set[String]]): (String, String) = {
@@ -91,22 +85,6 @@ object Day25 {
     }
 
     (start, end)
-  }
-
-  def findGroups(connections: Map[String, Set[String]]): (Set[String], Set[String]) = {
-    val queue = mutable.Queue(connections.head._1)
-    val visited = mutable.Set[String]()
-
-    while (queue.nonEmpty) {
-      val component = queue.dequeue()
-
-      if (!visited.contains(component)) {
-        visited.add(component)
-        queue.addAll(connections(component))
-      }
-    }
-
-    (visited.toSet, connections.keySet -- visited)
   }
 
   def parse(input: List[String]): Map[String, Set[String]] = {
